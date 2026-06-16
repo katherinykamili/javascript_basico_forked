c7b4ac4870e21f066df6d0ffd9f67828ed6a1e25
katherinykamili <katherinykc13@gmail.com>
2026-06-16 17:17:21 -0300

    adiciona exercícios de laços for e while

--- Katheriny/exerc. resolvidos/for.js
+++ Katheriny/exerc. resolvidos/for.js
@@ -1,7 +1,26 @@
+for (let i = 0; i < 5; i++) {
+    console.log("Bom dia")
+}
+
+for (let i = 0; i < 5; index++) {
+    soma = soma + 2
+    console.log(soma);
+}
 
 for (let i = 0; i <= 15;  i++) {
     if(i % 2 == 0){
         continue
-    } else {console.log("Número")}
-    
+    } else {console.log("Número ímpar: "+ i)}
+}
+
+for (let i = 0; i <= 15;  i++) {
+    if(i % 2 == 0){
+        console.log("Número par: " + i)
+    } else {console.log("Número ímpar: " + i)}
+}
+
+let opcao = "não"
+while(opcao != "sim"){
+    console.log("Olá, seja bem vindo!" + opcao)
+    opcao = prompt("Quer parar? 'sim' - para - 'não' - continua")
 }
