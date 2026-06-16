904157dd6e868b9536e62b12d00632a9c24bbfee
katherinykamili <katherinykc13@gmail.com>
2026-06-16 17:17:39 -0300

    adiciona exercícios de laços for e while

---
+++ Katheriny/pratica/pratica5.js
@@ -0,0 +1,19 @@
+// Aula 5 · Prática
+
+// 1 - Imprima números de 1 a 10 com for
+// Instrução: Use o laço for para exibir os números de 1 a 10 no console.
+// Dica: for (let i = 1; i <= 10; i++) { }
+
+for (let i = 1; i <= 10; i++) { 
+    console.log(i)
+}
+
+// 2 - Escreva while que soma até 100
+// Instrução: Use while para somar números de 1 até 100. Exiba o resultado final (que deve ser 5050).
+// Dica: Acumule os valores em uma variável soma.
+
+let contador = 0
+while (contador < 100) {
+    console.log(contador)
+    contador ++
+}
